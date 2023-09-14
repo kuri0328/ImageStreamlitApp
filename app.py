7834056caf63205f8d332b80bcd87ee7d21a5dee
kuri <kurimaru0328@gmail.com>
2023-09-15 00:20:46 +0900

    3rd commit

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
-from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
-from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
 from msrest.authentication import CognitiveServicesCredentials
 
-from array import array
-import os
 from PIL import Image
-import sys
-import time
 
 import json
 
